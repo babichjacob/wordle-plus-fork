e1cda7991523b8ec25171b12c9e111fc29f74fe3
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2023-07-03 22:18:31 -0400

    change base path to `/`

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { version } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wordle/",
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
